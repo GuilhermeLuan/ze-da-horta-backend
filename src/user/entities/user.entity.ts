da342b2b077451e6e176a67b227f9793fe92e894
Guilherme Luan <guilhermeluan@pm.me>
2025-05-19 17:08:07 -0300

    feat: create user entity and database config

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,46 @@
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { ClientProfile } from './client-profile.entity';
+
+export enum UserRole {
+  DELIVERY = 'entregador',
+  CLIENT = 'cliente',
+  PRODUCER = 'produtor',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole })
+  role: UserRole;
+  @OneToOne(() => ClientProfile, (clientProfile) => clientProfile.user, {
+    cascade: true,
+    nullable: true,
+    eager: true,
+  })
+  clientProfile?: ClientProfile;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+}
