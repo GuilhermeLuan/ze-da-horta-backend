06d8af1d2beded16da09dc6394bb89d94567b45b
Guilherme Luan <guilhermeluan@pm.me>
2025-05-26 20:35:26 -0300

    feat: define relationship between product and store entities

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,23 @@
-export class Product {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Store } from '../../store/entities/store.entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column()
+  description: string;
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number;
+  @Column()
+  imageUrl: string;
+  @Column()
+  rating: number;
+  @Column()
+  category: string;
+
+  @ManyToOne(() => Store, (store) => store.products)
+  store: Store;
+}
