7ae264326c4804ba450d92a32038096445b39975
Guilherme Luan <guilhermeluan@pm.me>
2025-05-26 15:35:56 -0300

    feat: configure typeorm in store module

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { StoreController } from './store.controller';
+import { User } from '../user/entities/user.entity';
+import { Store } from './entities/store.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Store])],
   controllers: [StoreController],
   providers: [StoreService],
 })
